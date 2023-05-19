51c64e2d1175e1c5aa67378da38d2ea6701b26da
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2023-05-19 19:06:18 +0530

    build option in package.json

--- app.js
+++ app.js
@@ -8,34 +8,34 @@ app.use(express.json());
 app.post('/bfhl', (req, res) => {
     // Get the request data from the request body
     const { data } = req.body;
-
+  
     // Extract necessary fields from the request
     const full_name = 'john_doe';
     const dob = '17091999';
     const email = 'john@xyz.com';
     const roll_number = 'ABCD123';
-
+  
     // Filter numbers and alphabets from the data array
     const numbers = data.filter(item => !isNaN(item));
     const alphabets = data.filter(item => isNaN(item));
-
+  
     // Generate the user ID using the provided fields
     const user_id = `${full_name}_${dob}`;
-
+  
     // Create the response object
     const response = {
-        is_success: true,
-        user_id,
-        email,
-        roll_number,
-        numbers,
-        alphabets
+      is_success: true,
+      user_id,
+      email,
+      roll_number,
+      numbers,
+      alphabets
     };
-
+  
     // Return the response as JSON
     res.json(response);
-});
-
+  });
+  
 
 // Route: /bfhl | Method: GET
 app.get('/bfhl', (req, res) => {
